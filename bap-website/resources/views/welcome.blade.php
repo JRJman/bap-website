c470a9ddac7bd56c9382e488b44917148b95fd0c
JRJman <41746794+JRJman@users.noreply.github.com>
2019-11-18 12:24:18 +0100

    naam aanpassen

--- bap-website/resources/views/welcome.blade.php
+++ bap-website/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome Joep Janssen
                 </div>
 
                 <div class="links">
