250057c45a8b402359cc6fe436ad9e9c227c618d
JRJman <41746794+JRJman@users.noreply.github.com>
2019-11-21 09:45:47 +0100

    2.08A opdrachten af

---
+++ bap-website/config/bap.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Ma Student name
+    |--------------------------------------------------------------------------
+    |
+    | This value gives the name of the one that made this program.
+    |
+    */
+
+    'student' => env('MA_STUDENT_NAME','MA Student'),
+];
